d8d9911ce48ddac8d3ba6146f9529f86f58a078b
SaaaS04 <saulo.marcel@souunit.com.br>
2026-06-08 01:30:29 -0300

    Início do laço for

---
+++ 05.Arrays/for_1.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 6, 11, 910];
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log(i, numeros[i]);
+}
+for(let i = 1; i < numeros.length; i += 2) {
+    console.log(i, numeros[i]);
+}
+
