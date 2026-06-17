7c9c04fc29741ac76a5311d3d73148a15b3779a8
SaaaS04 <saulo.marcel@souunit.com.br>
2026-06-17 00:56:20 -0300

    Metodos de array 02 e 03

---
+++ metodos_3.js
@@ -0,0 +1,17 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+
+
+function cadaElemento(element, index, array) {
+    console.log(element, index, array)
+}
+numeros.forEach(cadaElemento)
+ 
+// for(let n of numeros) {
+//     console.log(n)
+// }
+
+
+// for (let i = 0; i < numeros.length; i++) {
+//     console.log(numeros[i]);
+// }
+
