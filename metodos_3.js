50859b0f3201aa6938f7814eb0aad1b685ca576f
SaaaS04 <saulo.marcel@souunit.com.br>
2026-06-21 15:20:39 -0300

    Metodos_3

--- metodos_3.js
+++ metodos_3.js
@@ -1,10 +1,18 @@
-const numeros = [1, 2, 3, 4, 5, 6];
+const numeros = [10, 20, 30, 40];
 
 
 function cadaElemento(element, index, array) {
     console.log(element, index, array)
 }
 numeros.forEach(cadaElemento)
+
+numeros.forEach(function(el){
+    console.log(el)
+})
+
+numeros.forEach(function(_, i){
+    console.log(i)
+})
  
 // for(let n of numeros) {
 //     console.log(n)
