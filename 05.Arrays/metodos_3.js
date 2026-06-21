60a0a6bd13ef9bf68a898a2383782f0adf93afe7
SaaaS04 <saulo.marcel@souunit.com.br>
2026-06-21 17:25:51 -0300

    Começo do módulo de objetos e final de Arrays

--- 05.Arrays/metodos_3.js
+++ 05.Arrays/metodos_3.js
@@ -22,4 +22,3 @@ numeros.forEach(function(_, i){
 // for (let i = 0; i < numeros.length; i++) {
 //     console.log(numeros[i]);
 // }
-
---
+++ 06. Objetos/basico_1.js
@@ -0,0 +1,17 @@
+const numeros = [1, 2, 3];
+const produto = {
+    nome: 'Bolo',
+    preco: 10.90,
+    sabor: 'chocolate'
+};
+
+console.log(numeros[0]);
+console.log(produto['nome']);
+console.log(produto['preco']);
+console.log(produto['sabor']);
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.sabor);
+
+console.log(produto);
